afe3e4394847f2d40ecf110ce887a9597bd885b0
Andre Wohnsland <50302161+AndreWohnsland@users.noreply.github.com>
2024-06-11 19:52:54 +0200

    Make mypy happy

--- skill_plotter/preparator.py
+++ skill_plotter/preparator.py
@@ -1,11 +1,12 @@
 import json
-import jsonschema
 from pathlib import Path
-from typing import Any, Optional
-import typer
-import click
+from typing import Any
 
-from .utils import success_print, failure_print, info_print
+import click
+import jsonschema
+import typer
+
+from .utils import failure_print, info_print, success_print
 
 _APP_NAME = "skill-plotter"
 _app_dir = typer.get_app_dir(_APP_NAME)
@@ -18,7 +19,7 @@ _DEFAULT_CATEGORY = "default"
 
 # Validate the data against the schema
 def _validate_data(data: dict):
-    """Validates the given data to be compatible with the schema."""
+    """Validate the given data to be compatible with the schema."""
     # schema is {"skill_name": {"level": 3.5, "category": "default"}, ...}
     schema = {
         "type": "object",
@@ -39,7 +40,7 @@ def _validate_data(data: dict):
 
 
 def _get_target_file(file_name: str = DEFAULT_SKILL_FILE_NAME) -> Path:
-    """Returns the target file path."""
+    """Return the target file path."""
     return Path(_app_dir) / f"{file_name}.json"
 
 
@@ -48,7 +49,7 @@ def split_dict_evenly(d: dict, n: int) -> list[dict]:
     items = list(d.items())
     chunk_size = len(items) // n
     remainder = len(items) % n
-    splitted = []
+    splitted: list[dict] = []
     start = 0
     for i in range(n):
         end = start + chunk_size
@@ -58,15 +59,17 @@ def split_dict_evenly(d: dict, n: int) -> list[dict]:
         start = end
     # append empty element if it is not evenly splitted
     max_len = len(splitted[0])
-    for i in splitted:
-        if len(i) < max_len:
-            i[""] = 0
+    for item in splitted:
+        if len(item) < max_len:
+            item[""] = 0
     return splitted
 
 
 def sort_skills_by_category(skills: dict[str, dict[str, Any]]) -> dict[str, dict[str, Any]]:
-    """Sorts the given skills by category.
-    Returns a sorted dict without the categories"""
+    """Sort the given skills by category.
+
+    Returns a sorted dict without the categories
+    """
     # skills are of struct {skill: {category: str, level: float}}
     # the skills can have more attributes, but we are only interested in those both
     # sort by category and then by level
@@ -77,24 +80,24 @@ def sort_skills_by_category(skills: dict[str, dict[str, Any]]) -> dict[str, dict
 
 
 def reduce_data(data: dict[str, dict[str, Any]]) -> dict[str, float]:
-    """Reduces the dict to only key and value"""
+    """Reduce the dict to only key and value."""
     return {k: v["level"] for k, v in data.items()}
 
 
 def read_file(file_name: str = DEFAULT_SKILL_FILE_NAME) -> dict[str, dict[str, Any]]:
-    """Reads the file from the given path and returns a dict.
+    """Read the file from the given path and returns a dict.
+
     If the file path does not exist returns an empty dict.
     """
     skill_file = _get_target_file(file_name)
     if not skill_file.exists():
         return {}
-    with open(skill_file, "r", encoding="utf-8") as json_file:
-        data = json.load(json_file)
-    return data
+    with open(skill_file, encoding="utf-8") as json_file:
+        return json.load(json_file)
 
 
 def write_file(data: dict, file_name: str = DEFAULT_SKILL_FILE_NAME) -> None:
-    """Writes the given dict to the file path."""
+    """Write the given dict to the file path."""
     skill_file = _get_target_file(file_name)
     if not skill_file.exists():
         skill_file.touch()
@@ -103,7 +106,8 @@ def write_file(data: dict, file_name: str = DEFAULT_SKILL_FILE_NAME) -> None:
 
 
 def add_skill(skill: str, level: float, category: str = _DEFAULT_CATEGORY, file_name: str = DEFAULT_SKILL_FILE_NAME):
-    """Adds the skill to the skill list.
+    """Add the skill to the skill list.
+
     If it already exists, the level will be overwritten.
     """
     data = read_file(file_name)
@@ -114,7 +118,7 @@ def add_skill(skill: str, level: float, category: str = _DEFAULT_CATEGORY, file_
 
 def interactive_add_skill(
     file_name: str = DEFAULT_SKILL_FILE_NAME,
-    used_category: Optional[str] = None,
+    used_category: str | None = None,
 ):
     """Interactively adds skills to the skill list."""
     info_print(f"Using interactive mode, use Ctrl+C to exit, skills will be added to {file_name}")
@@ -127,10 +131,7 @@ def interactive_add_skill(
             level = typer.prompt("Enter level [0-10]", type=float)
             if level <= 0 or level > 10:
                 failure_print("Level must be between 0 and 10 and not 0")
-        if used_category is None:
-            category = typer.prompt("Enter category", default=_DEFAULT_CATEGORY)
-        else:
-            category = used_category
+        category = typer.prompt("Enter category", default=_DEFAULT_CATEGORY) if used_category is None else used_category
         # this should usually not be happening, but just in case
         if not category:
             category = _DEFAULT_CATEGORY
@@ -140,7 +141,7 @@ def interactive_add_skill(
 
 
 def remove_skill(skill: str, file_name: str = DEFAULT_SKILL_FILE_NAME):
-    """Removes the skill from the skill list."""
+    """Remove the skill from the skill list."""
     data = read_file(file_name)
     if skill in data:
         del data[skill]
@@ -163,8 +164,10 @@ def interactive_remove(file_name: str = DEFAULT_SKILL_FILE_NAME):
 
 
 def delete_group(group: str):
-    """Deletes the given group file (json) from the app dir
-    Informs user if it was successful or not"""
+    """Delete the given group file (json) from the app dir.
+
+    Informs user if it was successful or not
+    """
     skill_file = _get_target_file(group)
     if skill_file.exists():
         skill_file.unlink()
@@ -176,7 +179,9 @@ def delete_group(group: str):
 
 def list_all_groups():
     """Search the app dir for all existing groups and print them.
-    Valid files are json ones"""
+
+    Valid files are json ones
+    """
     files = Path(_app_dir).glob("*.json")
     info_print("Existing groups:")
     for file in files:
@@ -186,7 +191,7 @@ def list_all_groups():
 
 
 def list_all_skills(group: str):
-    """List all skills of the group"""
+    """List all skills of the group."""
     data = read_file(group)
     if len(data) == 0:
         failure_print(f"No skills found in group {group}, it probably does not exist!")
@@ -207,7 +212,7 @@ def list_all_skills(group: str):
 
 
 def export_skills_to_file(export_name: str, skill_group: str):
-    """Exports the skills of the given group to a file."""
+    """Export the skills of the given group to a file."""
     file_to_export = _get_target_file(skill_group)
     if not file_to_export.exists():
         failure_print(f"Group {skill_group} does not exist, only those are valid:")
@@ -219,7 +224,7 @@ def export_skills_to_file(export_name: str, skill_group: str):
 
 
 def import_skills_from_file(import_file: Path, skill_group: str, overwrite: bool):
-    """Imports the given file to the skill group or creates a new one."""
+    """Import the given file to the skill group or creates a new one."""
     # check if import file is valid (exists and is json file)
     if not import_file.exists():
         failure_print(f"Import file {import_file} does not exist")
@@ -228,7 +233,7 @@ def import_skills_from_file(import_file: Path, skill_group: str, overwrite: bool
         failure_print(f"Import file {import_file} is not a JSON file")
         return
 
-    with open(import_file, "r", encoding="utf-8") as json_file:
+    with open(import_file, encoding="utf-8") as json_file:
         import_data = json.load(json_file)
 
     if not _validate_data(import_data):
